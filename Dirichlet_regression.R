375ec1308ab0e5f276b04f4851756638b33618b1
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2023-06-02 17:23:19 -0400

    k=3

--- Dirichlet_regression.R
+++ Dirichlet_regression.R
@@ -52,43 +52,31 @@ summary(model)
 #### given that we expect there to be environmental differences, and we want to be able to account for them, we need a hierarchical model that does this same thing, but with hyper parameters for each of the environmental variables
 ###let's simulate 5 populations (200 individuals each), and the only thing that we're gonna change is the relative q score distributins 
 
-q_scores_p1<-rdirichlet(n=200, alpha=c(1,2,3,4,5))
-q_scores_p2<-rdirichlet(n=200, alpha=c(5,4,3,2,1))
-q_scores_p3<-rdirichlet(n=200, alpha=c(3,3,3,3,3))
-q_scores_p4<-rdirichlet(n=200, alpha=c(11,1,1,1,1))
-q_scores_p5<-rdirichlet(n=200, alpha=c(4,4,4,2,1))
+q_scores_p1<-rdirichlet(n=350, alpha=c(1,2,3,4,5))
+q_scores_p2<-rdirichlet(n=350, alpha=c(5,4,3,2,1))
+q_scores_p3<-rdirichlet(n=300, alpha=c(3,3,3,3,3))
 
-q_scores_allpops<-rbind.data.frame(q_scores_p1, q_scores_p2, q_scores_p3, q_scores_p4, q_scores_p5)
-names(q_scores_allpops)<-c("q1", "q2", "q3", "q4", "q5")
-en1_pop1<-1+5*q_scores_p1[,1]+15*q_scores_p1[,2]+rnorm(200, 0, 5)
-en2_pop1<-1+5*q_scores_p1[,2]+15*q_scores_p1[,5]+rnorm(200, 0, 5)
-en3_pop1<-1+5*q_scores_p1[,3]+15*q_scores_p1[,4]+rnorm(200, 0, 5)
-en4_pop1<-rnorm(200, 0, 5)
-en5_pop1<-rnorm(200, 0, 5)
+q_scores_allpops<-rbind.data.frame(q_scores_p1, q_scores_p2, q_scores_p3)
+names(q_scores_allpops)<-c("q1", "q2", "q3")
+en1_pop1<-1+5*q_scores_p1[,1]+15*q_scores_p1[,2]+rnorm(350, 0, 5)
+en2_pop1<-1+5*q_scores_p1[,2]+15*q_scores_p1[,5]+rnorm(350, 0, 5)
+en3_pop1<-1+5*q_scores_p1[,3]+15*q_scores_p1[,4]+rnorm(350, 0, 5)
+en4_pop1<-rnorm(350, 0, 5)
+en5_pop1<-rnorm(350, 0, 5)
 
-en1_pop2<-1+5*q_scores_p1[,1]+15*q_scores_p1[,2]+rnorm(200, 0, 5)
-en2_pop2<-1+5*q_scores_p1[,2]+15*q_scores_p1[,5]+rnorm(200, 0, 5)
-en3_pop2<-1+5*q_scores_p1[,3]+15*q_scores_p1[,4]+rnorm(200, 0, 5)
-en4_pop2<-rnorm(200, 0, 5)
-en5_pop2<-rnorm(200, 0, 5)
+en1_pop2<-1+5*q_scores_p2[,1]+15*q_scores_p2[,2]+rnorm(350, 0, 5)
+en2_pop2<-1+5*q_scores_p2[,2]+15*q_scores_p2[,5]+rnorm(350, 0, 5)
+en3_pop2<-1+5*q_scores_p2[,3]+15*q_scores_p2[,4]+rnorm(350, 0, 5)
+en4_pop2<-rnorm(350, 0, 5)
+en5_pop2<-rnorm(350, 0, 5)
 
-en1_pop3<-rnorm(200, 0, 5)
-en2_pop3<-rnorm(200, 0, 5)
-en3_pop3<-1+5*q_scores_p1[,1]+15*q_scores_p1[,2]+rnorm(200, 0, 5)
-en4_pop3<-1+5*q_scores_p1[,3]+15*q_scores_p1[,4]+rnorm(200, 0, 5)
-en5_pop3<-1+5*q_scores_p1[,1]+10*q_scores_p1[,2]+15*q_scores_p1[,3]+20*q_scores_p1[,4]+25*q_scores_p1[,4]+rnorm(200, 0, 5)
+en1_pop3<-1+5*q_scores_p3[,1]+15*q_scores_p3[,2]+rnorm(300, 0, 5)
+en2_pop3<-1+5*q_scores_p3[,2]+15*q_scores_p3[,5]+rnorm(300, 0, 5)
+en3_pop3<-1+5*q_scores_p3[,3]+15*q_scores_p3[,4]+rnorm(300, 0, 5)
+en4_pop3<-rnorm(300, 0, 5)
+en5_pop3<-rnorm(300, 0, 5)
 
-en1_pop4<-rnorm(200, 0, 5)
-en2_pop4<-rnorm(200, 0, 5)
-en3_pop4<-1+5*q_scores_p1[,1]+15*q_scores_p1[,2]+rnorm(200, 0, 5)
-en4_pop4<-1+5*q_scores_p1[,3]+15*q_scores_p1[,4]+rnorm(200, 0, 5)
-en5_pop4<-1+5*q_scores_p1[,1]+10*q_scores_p1[,2]+15*q_scores_p1[,3]+20*q_scores_p1[,4]+25*q_scores_p1[,4]+rnorm(200, 0, 5)
 
-en1_pop5<-rnorm(200, 0, 5)
-en2_pop5<-rnorm(200, 0, 5)
-en3_pop5<-1+5*q_scores_p1[,1]+15*q_scores_p1[,2]+rnorm(200, 0, 5)
-en4_pop5<-1+5*q_scores_p1[,3]+15*q_scores_p1[,4]+rnorm(200, 0, 5)
-en5_pop5<-1+5*q_scores_p1[,1]+10*q_scores_p1[,2]+15*q_scores_p1[,3]+20*q_scores_p1[,4]+25*q_scores_p1[,4]+rnorm(200, 0, 5)
 
 pop1<-cbind.data.frame(en1_pop1, en2_pop1, en3_pop1, en4_pop1, en5_pop1)
 names(pop1)<-c("en1", "en2", "en3", "en4", "en5")
@@ -96,14 +84,11 @@ pop2<-cbind.data.frame(en1_pop2, en2_pop2, en3_pop2, en4_pop2, en5_pop2)
 names(pop2)<-c("en1", "en2", "en3", "en4", "en5")
 pop3<-cbind.data.frame(en1_pop3, en2_pop3, en3_pop3, en4_pop3, en5_pop3)
 names(pop3)<-c("en1", "en2", "en3", "en4", "en5")
-pop4<-cbind.data.frame(en1_pop4, en2_pop4, en3_pop4, en4_pop4, en5_pop4)
-names(pop4)<-c("en1", "en2", "en3", "en4", "en5")
-pop5<-cbind.data.frame(en1_pop5, en2_pop5, en3_pop5, en4_pop5, en5_pop5)
-names(pop5)<-c("en1", "en2", "en3", "en4", "en5")
 
-env_allpops<-abs(rbind.data.frame(pop1, pop2, pop3, pop4, pop5)) ### getting a stan error that dirichlet predictors need to be positive. Not sure why that would be (worth running down, but doing this for now to try to get the model to work)
+
+env_allpops<-rbind.data.frame(pop1, pop2, pop3) 
 hierarchical_data<-cbind.data.frame(q_scores_allpops, env_allpops)
-hierarchical_data$pop<-c(rep(1, 200), rep(2, 200), rep(3, 200), rep(4, 200), rep(5, 200))
+hierarchical_data$pop<-c(rep(1, 350), rep(2, 350), rep(3, 300))
 
 
 #### The STAN PART ####
@@ -119,8 +104,8 @@ N<-length(hierarchical_data[,1]) ###how many observations are there
 K<-5 ### how many environmental variables are there?
 J<-length(unique(hierarchical_data$pop)) ###how many populations are there?
 pop<-hierarchical_data$pop
-x<-hierarchical_data[,6:10]
-y<-hierarchical_data[,1:5]
+x<-hierarchical_data[,4:8]
+y<-hierarchical_data[,1:3]
 
 
 ### run the model
